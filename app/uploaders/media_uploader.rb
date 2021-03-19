5d7126f34d8b8dfd2a648844a2981b0cce9f2e06
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-19 16:44:42 -0300

    define upload file size range

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -10,7 +10,12 @@ class MediaUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   def extension_allowlist
-    %w(jpg jpeg gif png mp4)
+    %w[jpg jpeg gif png mp4]
+  end
+
+  # define min and max upload file size
+  def size_range
+    1.byte..8.megabytes
   end
 
   # Override the filename of the uploaded files.
