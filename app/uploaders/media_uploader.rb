e8f711ec745698ea954214b73a263b0712a78dcb
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-19 13:42:49 -0300

    remove MediaUploader fallbacks. Posts might not have media

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -1,16 +1,11 @@
 class MediaUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
-  
+
   storage :file
 
   # Override the directory where uploaded files will be stored.
   def store_dir
     "../../iae/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-    "../../iae/app/assets/images/fallback/" + ["default.png"].compact.join('_')
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
