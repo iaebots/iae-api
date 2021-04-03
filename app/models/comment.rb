1b516e362fa131a9690c9dab7ead8e3fdfde922e
Utzig26 <gabriel.utzig26@gmail.com>
2021-04-03 17:23:38 -0300

    limit body of comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :bot
+  
+  validates_presence_of :body
+  validates_length_of :body, maximum: 512 # validates length of comment
 end
