eb64342f243cf1ead7fcda7ef74deebf87aa41dc
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-07 14:49:45 -0300

    refactor ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,31 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-    include ActionController::HttpAuthentication::Token::ControllerMethods
-   
-    # Add a before_action to authenticate all requests.
-    # Move this to subclassed controllers if you only
-    # want to authenticate certain methods.
-    #rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-    #rescue_from AbstractController::ActionNotFound, :with => :render_404
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
-    before_action :authenticate
-  
-    protected
-  
-    # Authenticate the user with token based authentication
-    def authenticate
-      authenticate_token || render_unauthorized
-    end
-  
-    def authenticate_token
-      authenticate_with_http_token do |token_key, options|
-        token_key = token_key.split(/(?:api_key=)(\w*)(?: api_secret=)(\w*)/)
-        @current_bot = Bot.select(:id, :username, :name, :bio, :created_at).find_by(api_key: token_key[1], api_secret: token_key[2])
-      end
-    end
-  
-    def render_unauthorized(realm = "Application")
-      self.headers["WWW-Authenticate"] = %(Token_id realm="#{realm}")
-      render json: {status: 'ERROR', message:'401 - bad credentials'}, status: :unauthorized
-    end
+  before_action :authenticate
 
-    def render_404
-      render json: {status: 'ERROR', message:'404 - record not found'}, status: :not_found
+  protected
+
+  # Authenticate the user with token based authentication
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token_key, _options|
+      token_key = token_key.split(/api_key=(\w*) api_secret=(\w*)/)
+      @current_bot = Bot.select(:id, :username, :name, :bio, :created_at).find_by(api_key: token_key[1],
+                                                                                  api_secret: token_key[2])
     end
-    
+  end
+
+  def render_unauthorized(realm = 'Application')
+    headers['WWW-Authenticate'] = %(Token_id realm="#{realm}")
+    render json: { status: 'error', message: 'Bad credentials' }, status: :unauthorized
+  end
+
+  def render_404
+    render json: { status: 'error', message: 'Record not found' }, status: :not_found
+  end
 end
-
