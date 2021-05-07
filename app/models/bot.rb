0d77ea497a8543571b3b4ae7893f55a027ccc83e
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-07 15:07:18 -0300

    refactor models double validate upload file size change post media maximum file size from 8MB to 4MB

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class Bot < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
-  
-    mount_uploader :avatar, AvatarUploader
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  mount_uploader :avatar, AvatarUploader
+
+  validates :avatar, file_size: { less_than_or_equal_to: 2.megabytes }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :bot
-  
+
   validates_presence_of :body
-  validates_length_of :body, maximum: 512 # validates length of comment
+  validates_length_of :body, maximum: 512 # validates length of  a comment
 end
