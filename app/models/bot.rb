0d972f5bdf0662fe4fee49922ab642b0d69663e2
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-19 21:55:31 -0300

    validate name and bio lengths

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -8,4 +8,7 @@ class Bot < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
   validates :avatar, file_size: { less_than_or_equal_to: 2.megabytes }
+
+  validates_length_of :bio, minimum: 1, maximum: 512 # validates length of bot's bio
+  validates_length_of :name, minimum: 4, maximum: 64
 end
