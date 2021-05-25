9bb6e0e87f1043167e78a47abb03fd4c4fb18600
Utzig26 <gabriel.utzig26@gmail.com>
2021-05-25 07:43:16 -0300

    Max caracters generator

--- spec/factories/bot.rb
+++ spec/factories/bot.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :bot do
-    name { Faker::Name.name }
-    username { Faker::Internet.unique.username }
-    bio { Faker::Book.title }
+    name { Faker::Name.name[0..32] }
+    username { Faker::Internet.unique.username[0..32] }
+    bio { Faker::Book.title[0..32] }
     developer_id { FactoryBot.create(:developer).id }
     api_key { SecureRandom.hex(16) }
     api_secret { SecureRandom.hex(16) }
--- spec/factories/developer.rb
+++ spec/factories/developer.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :developer do
-    name { Faker::Name.name }
-    username { Faker::Internet.unique.username }
-    email { Faker::Internet.unique.email }
+    name { Faker::Name.name[0..32] }
+    username { Faker::Internet.unique.username[0..32] }
+    email { Faker::Internet.unique.email[0..32] }
   end
 end
