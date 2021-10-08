b65f3f803e534a67ec0a0b698a9e69d393d18c9a
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-10-08 11:20:24 -0300

    improve API responses on PostsController and BotsController add documentation

--- app/controllers/api/v1/bots_controller.rb
+++ app/controllers/api/v1/bots_controller.rb
@@ -2,51 +2,60 @@
 
 module Api
   module V1
+    # This controller consits of all possible requests can be done via API on Bots.
+    # The allowed requests are GET and PUT.
+    # GET (index): return all existing bots.
+    # GET (show): return a single bot given its username.
+    # PUT: allow updating bots' name, bio, avatar and/or cover image. Other attributes cannot be update via API.
+    # For PUT requests, the request is validated using required_authorization! method.
     class BotsController < ApplicationController
       before_action :find_bot, only: %i[show update]
       before_action :require_authorization!, only: %i[update]
 
-      # GET /resource
+      # GET /bots
+      # Returns all existing bots.
       def index
-        @bots = Bot.all.select(:id, :username, :name, :bio, :created_at, :name, :developer_id, :verified, :avatar)
+        @bots = Bot.all.select(:id, :name, :username, :bio, :verified, :avatar_data, :cover_data,
+                               :developer_id, :created_at)
         render json: { status: 'success', message: 'All bots loaded', data: @bots }, status: :ok
       end
 
-      # GET /resource/:username
+      # GET /bots/:username
+      # Returns a single bot given its username.
       def show
-        if @bot
-          render json: { status: 'success', message: 'Bot loaded', data: @bot }, status: :ok
-        else
-          render json: { status: 'error', message: 'Bot not found', data: nil }, status: :not_found
-        end
+        render json: { status: 'success', message: 'Bot loaded', data: @bot }, status: :ok if @bot
       end
 
-      # PUT /resource/:username
+      # PUT /bots/:username
+      # Updates attributes such as name, bio, avatar and cover.
       def update
         if @bot.update(bot_params)
           render json: { status: 'success', message: 'Bot updated', data: @bot }, status: :ok
         else
-          render json: { status: 'error', message: 'Bot not updated', errors: @bot.errors },
-                 status: :unprocessable_entity
+          render json: { status: 'error', message: 'Bot not updated', errors: @bot.errors.full_messages },
+                 status: :bad_request
         end
       end
 
       private
 
-      # Define what params are permitted
+      # Defines what params are permitted (for PUT requests)
       def bot_params
-        params.permit(:name, :bio, :avatar)
+        params.permit(:name, :bio, :avatar, :cover)
       end
 
-      # Find a bot by username
+      # Finds a bot by username (friendly_id)
       def find_bot
-        @bot = Bot.select(:id, :username, :name, :bio, :created_at, :name, :developer_id, :verified,
-                          :avatar_data).find_by(username: params[:username])
+        @bot = Bot.select(:id, :name, :username, :slug, :bio, :verified, :avatar_data, :cover_data,
+                          :developer_id, :created_at).friendly.find(params[:id])
       end
 
+      # Checks if bot requesting PUT is trying to update itself. Otherwise returns 401.
       def require_authorization!
+        return if @current_bot == @bot
+
         render json: { status: 'error', message: 'Resource does not belong to you' },
-               status: :unauthorized unless @current_bot == @bot
+               status: :unauthorized
       end
     end
   end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,56 +2,72 @@
 
 module Api
   module V1
+    # This controller consits of all possible requests can be done via API on Posts.
+    # The allowed requests are GET, POST and DELETE.
+    # GET: return a single post, containing its comments and likes, given its id and its owner's (bot's) username.
+    # POST: create a post given a body and/or media.
+    # DELETE: deletes a post.
+    # DELETE requests are validated by require_authorization! method.
     class PostsController < ApplicationController
-      before_action :set_post, only: %i[show destroy]
+      before_action :find_post, only: %i[show destroy]
       before_action :require_authorization!, only: :destroy
       before_action :set_response, only: :show
       before_action :new_post, only: :create
 
-      # GET /:username/resource/:id
+      # POST /posts
+      # Creates a post given a body and/or media. Body is optional if media is present.
+      def create
+        if @post.save
+          @post = @post.attributes.slice('id', 'bot_id', 'body', 'media_data', 'created_at')
+          render json: { status: 'success', message: 'Post created', data: @post }, status: :created
+        else
+          render json: { status: 'error', message: 'Post not created', errors: @post.errors.full_messages },
+                 status: :bad_request
+        end
+      end
+
+      # GET /:username/posts/:id
+      # Returns a single post (and its content) given its id and owner's (bot's) username. 
       def show
         render json: { status: 'success', message: 'Post loaded', data: @response }, status: :ok
       end
 
-      # POST /resource
-      def create
-        if @post.save
-          render json: { status: 'success', message: 'Post created', data: @post }, status: :created
-        else
-          render json: { status: 'error', message: 'Post not created', data: nil }, status: :bad_request
-        end
-      end
-
-      # DELETE /:username/resource/:id
+      # DELETE /:username/posts/:id
+      # Deletes a single post (and its content) given its id and owner's (bot's) username. 
       def destroy
         if @post.destroy
           render json: { status: 'success', message: 'Post deleted', data: @post }, status: :accepted
         else
-          render json: { status: 'error', message: 'Post not deleted', data: nil }, status: :unprocessable_entity
+          render json: { status: 'error', message: 'Post not deleted', errors: @post.errors.full_messages },
+                 status: :unprocessable_entity
         end
       end
 
       private
 
-      # Define permitted params
+      # Defines permitted params (for POST method)
       def post_params
         params.permit(:body, :media, :username)
       end
 
-      # Define new post content
+      # Defines new post content
       def new_post
         @post = Post.new(post_params)
         @post.bot = @current_bot
       end
 
-      # Set post and its content (comment and likes)
-      def set_post
-        @post = Post.find(params[:id])
-        @comments = @post.comments.where(commentable_id: @post.id, commentable_type: 'Post').first
+      # Finds post and its content (comment and likes) by its id and owner's (bot's) username.
+      def find_post
+        @post = Post.joins(:bot).where("bots.username = '#{params[:username]}'").select('posts.id, posts.body,
+                                        bots.id as bot_id, bots.username as bot_username, posts.media_data,
+                                        posts.created_at').find(params[:id])
+        @comments = @post.comments.where(commentable_id: @post.id, commentable_type: 'Post')
+                         .select(:id, :commentable_type, :commenter_id, :commenter_type, :body, :created_at)
         @likes = @post.likes.count
       end
 
-      # Set a response for GET /:username/resource/:id
+      # Set a response for GET /:username/posts/:id
+      # The responde contains the post data and its content (comments and likes).
       def set_response
         @response = {
           post: @post,
@@ -60,6 +76,7 @@ module Api
         }
       end
 
+      # Checks if bot requesting DELETE owns the post before action. Otherwise returns 401.
       def require_authorization!
         return if @current_bot == @post.bot
 
