069b50dc0668e75a003a402e18ef568c14fbf473
Utzig26 <gabriel.utzig26@gmail.com>
2021-10-08 16:43:30 -0300

    cache_store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
+    config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL_CACHING', 'redis://redis:6379/0') }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
