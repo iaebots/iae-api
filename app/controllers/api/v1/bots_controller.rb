d3dad2c14d9983c6034bb560ddd5822d3861d36d
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-10-10 11:52:56 -0300

    paginate BotsController responses (#120) bots#index paginated bots#show returns bot posts, paginated

--- app/controllers/api/v1/bots_controller.rb
+++ app/controllers/api/v1/bots_controller.rb
@@ -4,26 +4,28 @@ module Api
   module V1
     # This controller consits of all possible requests can be done via API on Bots.
     # The allowed requests are GET and PUT.
-    # GET (index): return all existing bots.
+    # GET (index): return all existing bots, paginated.
     # GET (show): return a single bot given its username.
     # PUT: allow updating bots' name, bio, avatar and/or cover image. Other attributes cannot be update via API.
     # For PUT requests, the request is validated using required_authorization! method.
     class BotsController < ApplicationController
       before_action :find_bot, only: %i[show update]
+      before_action :set_response, only: :show
       before_action :require_authorization!, only: %i[update]
 
       # GET /bots
-      # Returns all existing bots.
+      # Returns all existing bots, paginated.
       def index
-        @bots = Bot.all.select(:id, :name, :username, :bio, :verified, :avatar_data, :cover_data,
-                               :developer_id, :created_at)
+        @bots = Bot.paginate(page: params[:page], per_page: max_page)
+                   .all.select(:id, :name, :username, :bio, :verified, :avatar_data, :cover_data, :developer_id,
+                               :created_at)
         render json: { status: 'success', message: 'All bots loaded', data: @bots }, status: :ok
       end
 
       # GET /bots/:username
-      # Returns a single bot given its username.
+      # Returns a single bot and its posts paginated given its username.
       def show
-        render json: { status: 'success', message: 'Bot loaded', data: @bot }, status: :ok if @bot
+        render json: { status: 'success', message: 'Bot loaded', data: @response }, status: :ok if @bot
       end
 
       # PUT /bots/:username
@@ -48,6 +50,28 @@ module Api
       def find_bot
         @bot = Bot.select(:id, :name, :username, :slug, :bio, :verified, :avatar_data, :cover_data,
                           :developer_id, :created_at).friendly.find(params[:id])
+      end
+
+      # Set response with bot's data and its posts
+      def set_response
+        @posts = @bot.posts.paginate(page: params[:page], per_page: max_page)
+                     .select(:id, :body, :media_data, :created_at)
+        @response = {
+          bot: @bot,
+          posts: @posts,
+          total_pages: @posts.total_pages
+        }
+      end
+
+      # Checks if max_page param is present and is smaller than 16
+      # If present and smaller than 16, returns params[:max_page]
+      # Else, returns default 16 as max_page
+      def max_page
+        if params[:max_page] && params[:max_page].to_i < 16
+          params[:max_page]
+        else
+          16
+        end
       end
 
       # Checks if bot requesting PUT is trying to update itself. Otherwise returns 401.
